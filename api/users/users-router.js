4bbeae0a40452ab88f39b4f6994f06f68dd8e58c
Zach-Morris <zach-morris@lambdastudents.com>
2021-07-07 16:30:50 -0400

    More RouterEndpoints

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -2,10 +2,9 @@ const express = require('express');
 
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
-const Post = require('../posts/posts-model.js')
-const express = require('express')
-
+const Posts = require('../posts/posts-model.js')
 const Users = require('./users-model.js')
+
 const { 
   logger, 
   validateUserId, 
@@ -28,6 +27,11 @@ router.get('/', (req, res, next) => {
 
 router.get('/:id', validateUserId, (req, res, next) => {
   // RETURN THE USER OBJECT
+  Users.getById(req.params.id)
+    .then(user => {
+      res.status(200).json(user)
+    })
+    .catch(next)
   // this needs a middleware to verify user id
 });
 
@@ -54,25 +58,41 @@ router.put('/:id', validateUserId, validateUser, (req, res, next) => {
   // and another middleware to check that the request body is valid
 });
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', validateUserId, (req, res, next) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   Users.remove(req.params.id)
     .then(() => {
-      res.status(200).json({ message: ''})
+      res.status(200).json({ message: 'User Deleted'})
     })
     .catch(next)
   // this needs a middleware to verify user id
 });
 
-router.get('/:id/posts', (req, res, next) => {
+router.get('/:id/posts', validateUserId, (req, res, next) => {
   // RETURN THE ARRAY OF USER POSTS
+  Users.getUserPosts(req.params.id)
+    .then(posts => {
+      res.status(200).json(posts)
+    })
+    .catch(next)
   // this needs a middleware to verify user id
 });
 
-router.post('/:id/posts', (req, res, next) => {
+router.post('/:id/posts', validateUserId, validatePost, (req, res, next) => {
   // RETURN THE NEWLY CREATED USER POST
+  const postInfo = {...req.body, user_id: req.params.id}
+
+  Posts.insert(postInfo)
+    .then(post => {
+      res.status(201).json(post)
+    })
+    .catch(error => {
+      next(error)
+    })
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
 });
 
 // do not forget to export the router
+
+module.exports = router;
